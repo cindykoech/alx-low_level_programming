db38a65fbc955166e4752411cc9fca8b291f79d8
cindykoech <cindykoech@gmail.com>
2022-09-08 13:03:18 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - Printing
- *
- * Return:Always 0 (Succesful)
- */
+* main - Printing
+* 
+* Return:Always 0 (Succesful)
+*/
 int main(void)
 {
       puts("\"Programming is like building a multilingual puzzle");
