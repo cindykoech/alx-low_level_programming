2727ae867a9d087ff7506b10b05623774f21ff1b
cindykoech <cindykoech@gmail.com>
2022-09-10 12:38:11 -0700

    Alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main - Entrypoint
+*
+*Return: 0
+*/
+int main(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+if (ch != 'e' && ch != 'q')
+	putchar(ch);
+putchar('\n');
+return (0);
+}
