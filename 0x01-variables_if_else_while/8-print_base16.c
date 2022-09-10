6b66547a43bd5632979dc48aec3b1c8ebae07c47
cindykoech <cindykoech@gmail.com>
2022-09-10 13:10:25 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Return: o
+*/
+int main(void)
+{
+char c;
+for (c = 'a'; c <= '9'; c++)
+	putchar(c);
+for (c = 'a'; c <= 'f'; c++)
+	putchar(c);
+putchar('\n')
+return (0);
+}
