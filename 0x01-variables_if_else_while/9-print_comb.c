8667c5c78e956e1966cb7fe57fe37f7a11d5aa86
cindykoech <cindykoech@gmail.com>
2022-09-10 13:32:36 -0700

    Possible combinations

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,10 +8,15 @@ int main(void)
 {
 int c;
 for (c = 48; c <= 57; c++)
+{
 	putchar(c);
-if (c != 57)
+if (c == 57)
+{
+break;
+}
 	putchar(',');
         putchar(' ');
+}
 putchar('\n');
 return (0);
 }
