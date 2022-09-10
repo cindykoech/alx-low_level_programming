25fda4f80ceeaf646f9436dc543e83d181b64cc7
cindykoech <cindykoech@gmail.com>
2022-09-10 13:55:37 -0700

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,9 +7,9 @@
 int main(void)
 {
 char c;
-for (c = '48'; c <= '57'; c++)
+for (c = 'a'; c <= '9'; c++)
 	putchar(c);
-for (c = '97'; c <= '102'; c++)
+for (c = 'a'; c <= 'f'; c++)
 	putchar(c);
 putchar('\n');
 return (0);
