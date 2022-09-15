539d323aff38331d2c3e86251b2f7bc73421b096
cindykoech <cindykoech@gmail.com>
2022-09-15 16:54:12 -0700

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,14 +9,14 @@ int print_sign(int n)
 if (n > 0)
 {
 _putchar('+');
-return (1);	
+return (1);
 }
 else if (n < 0)
-{				
+{	
 _putchar('-');
-return (-1);					
+return (-1);
 }
-else					
+else
 {
 _putchar('0');
 return (0);
