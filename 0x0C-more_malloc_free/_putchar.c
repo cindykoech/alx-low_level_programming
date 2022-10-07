72d374bdab56d109e8714aa099741208dbf85f94
cindykoech <cindykoech@gmail.com>
2022-10-07 14:17:20 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/_putchar.c
+++ 0x0C-more_malloc_free/_putchar.c
@@ -1,7 +1,10 @@
+#include "main.h"
 #include <unistd.h>
+
 /**
 *_putchar - writes the character c to stdout
 *@c: the character to print
+*
 *Return: on success 1
 *On error -1 is returned and errno is set appropriately
 */
